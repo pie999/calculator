a8dcea1ea2524f848673e480bba44582588db9b0
pie999 <pietro.vergara@libero.it>
2022-05-31 19:05:25 +0200

    Implement addition

--- script.js
+++ script.js
@@ -17,3 +17,27 @@ function operate(op, a, b){
     else if (op == "*") multiply(a, b);
     else if (op == "/") divide(a, b);
 }
+
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const history = [];
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        history.push(button.textContent);
+        display.textContent += button.textContent;
+        if (history[history.length - 1] == "=")
+            display.textContent = doOperation();
+    });
+});
+
+function doOperation (){
+    history.forEach((char, i) => {
+        if (char == "+"){
+            console.table(history);
+            let n1 = history.slice(0, i).join("");
+            let n2 = history.slice(i+1, -1).join("");
+            result = +n1 + +n2;
+        }
+    });
+    return result;
+}
